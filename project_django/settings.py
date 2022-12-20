b4424ed76258b74602cf750d5057ce843f2c21e9
Daniel Christian Mandolang <74773920+danielcm585@users.noreply.github.com>
2022-12-20 11:42:19 +0700

    Try deploy to vercel

--- project_django/settings.py
+++ project_django/settings.py
@@ -15,6 +15,8 @@ import dj_database_url
 from dotenv import load_dotenv
 from pathlib import Path
 
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -106,8 +108,6 @@ WSGI_APPLICATION = 'project_django.wsgi.application'
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
-
-load_dotenv()
 
 DATABASES = {
     'default': {
